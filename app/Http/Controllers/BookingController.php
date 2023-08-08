cf4206d4f9e1f677d289b14a535e47ed61955089
Hari haran <darkkillarprince@gmail.com>
2023-08-08 19:30:25 +0530

    oneway

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Booking;
 use App\Models\Car;
 use App\Models\Contact;
@@ -21,10 +22,11 @@ class BookingController extends Controller
     //
     public function bookNowStep2()
     {
-        $cars=Car::orderBy('created_at', 'asc')->get();
+        $cars = Car::orderBy('created_at', 'asc')->get();
+        // dd($cars);
         return view('booking.bookingstep2')->with(compact('cars'));
     }
-    
+
     /**
      * Show the form for creating a new resource.
      *
@@ -32,49 +34,49 @@ class BookingController extends Controller
      */
     public function store(Request $request)
     {
-        if($request['oneway_round']){
-        $data = array(
-            'from_place' => $request['from_place1'],
-            'to_place' => $request['to_place1'],
-            'car_id' => $request['car_id'],
-            'oneway_round' => 'One way',
-            'charge_per_km' => $request['charge_per_km'],
-            'distance' => $request['distance'],
-            'amount' => $request['amount'],
-            'depart_date_time' => date("Y-m-d H:i:s", strtotime($request['depart_date1']." ".$request['depart_time1'])),
-            'cust_name' => $request['cust_name'],
-            'cust_email' => $request['cust_email'],
-            'cust_mbl' => $request['cust_mbl1'],
-            'pickup_add' => $request['from_place1'],
-            'drop_add' => $request['to_place1'],
-            'days' => $request['days2'],
-            'actual_amount' => $request['actualAmount'],
-            'driver_bata' => $request['driverBata']
-        );
-    }
-     
-    if($request['oneway_round1']){
-        $data = array(
-            'from_place' => $request['from_place2'],
-            'to_place' => $request['to_place2'],
-            'car_id' => $request['car_id2'],
-            'oneway_round' => 'Round',
-            'charge_per_km' => $request['charge_per_km2'],
-            'distance' => $request['distance2'],
-            'amount' => $request['amount2'],
-            'depart_date_time' => date("Y-m-d H:i:s", strtotime($request['depart_date2']." ".$request['depart_time2'])),
-            'cust_name' => $request['cust_name'],
-            'cust_email' => $request['cust_email'],
-            'cust_mbl' => $request['cust_mbl1'],
-            'pickup_add' => $request['pickUpAddress'],
-            'drop_add' => $request['dropAddress'],
-            'return_date_time' => $request['return_date'],
-            'days' => $request['days2'],
-            'actual_amount' => $request['actualAmount2'],
-            'driver_bata' => $request['driverBata2']
-        );
-        // print_r($data); die;
-    }
+        if ($request['oneway_round']) {
+            $data = array(
+                'from_place' => $request['from_place1'],
+                'to_place' => $request['to_place1'],
+                'car_id' => $request['car_id'],
+                'oneway_round' => 'One way',
+                'charge_per_km' => $request['charge_per_km'],
+                'distance' => $request['distance'],
+                'amount' => $request['amount'],
+                'depart_date_time' => date("Y-m-d H:i:s", strtotime($request['depart_date1'] . " " . $request['depart_time1'])),
+                'cust_name' => $request['cust_name'],
+                'cust_email' => $request['cust_email'],
+                'cust_mbl' => $request['cust_mbl1'],
+                'pickup_add' => $request['from_place1'],
+                'drop_add' => $request['to_place1'],
+                'days' => $request['days2'],
+                'actual_amount' => $request['actualAmount'],
+                'driver_bata' => $request['driverBata']
+            );
+        }
+
+        if ($request['oneway_round1']) {
+            $data = array(
+                'from_place' => $request['from_place2'],
+                'to_place' => $request['to_place2'],
+                'car_id' => $request['car_id2'],
+                'oneway_round' => 'Round',
+                'charge_per_km' => $request['charge_per_km2'],
+                'distance' => $request['distance2'],
+                'amount' => $request['amount2'],
+                'depart_date_time' => date("Y-m-d H:i:s", strtotime($request['depart_date2'] . " " . $request['depart_time2'])),
+                'cust_name' => $request['cust_name'],
+                'cust_email' => $request['cust_email'],
+                'cust_mbl' => $request['cust_mbl1'],
+                'pickup_add' => $request['pickUpAddress'],
+                'drop_add' => $request['dropAddress'],
+                'return_date_time' => $request['return_date'],
+                'days' => $request['days2'],
+                'actual_amount' => $request['actualAmount2'],
+                'driver_bata' => $request['driverBata2']
+            );
+            // print_r($data); die;
+        }
         $bookings = new Booking($data);
         $bookings->save();
 
@@ -96,22 +98,23 @@ class BookingController extends Controller
 
         // Notifying Users
 
-       if(!empty($request['cust_email'])){
-        // print_r($request['cust_email']); die;
-           Mail::to($request['cust_email'])->send(new BookingNotificationMail($data));
+        if (!empty($request['cust_email'])) {
+            // print_r($request['cust_email']); die;
+            Mail::to($request['cust_email'])->send(new BookingNotificationMail($data));
         }
         // Notifying Admin
         $adminMailAddress = env('MAIL_FROM_ADDRESS');
         // print_r($adminMailAddress); die;
         Mail::to($adminMailAddress)->send(new BookingNotificationToAdminMail($data));
         // Mail::to('dhivyashree.duskcoder@gmail.com', 'Admin')->send(new BookingNotificationToAdminMail($data));
-       
-        return redirect()->route('booking-result',[$bookings->id]);
+
+        return redirect()->route('booking-result', [$bookings->id]);
     }
     public function bookingResult($id)
     {
         //  $bookedData = Booking::with('driver')->with('vehicle')->find($id);
         $bookedData = Booking::find($id);
-        return view('booking.success',compact('bookedData'));
+        dd($bookedData);
+        return view('booking.success', compact('bookedData'));
     }
 }
--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -6,63 +6,69 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- <link rel="icon" type="image/png" href="uploads/favicon.png" /> -->
-    
+
     <!-- <title>Doracabsoffers</title> -->
-    <meta name="description" content=" Dora Cabs For One way,Outstation, Car Rental, Local & Airport Taxi - Hire Me Cabs @ 9990965965">
-    <meta name="keywords" content="One way cab,Outstation taxi, Car Rental service , Local taxi hire, Airport Taxi service">
+    <meta name="description"
+        content=" Dora Cabs For One way,Outstation, Car Rental, Local & Airport Taxi - Hire Me Cabs @ 9990965965">
+    <meta name="keywords"
+        content="One way cab,Outstation taxi, Car Rental service , Local taxi hire, Airport Taxi service">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('/assets/frontuser/css/style.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/frontuser/css/owl.carousel.min.css')}}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/frontuser/css/owl.theme.default.min.css')}}" />
+    <link rel="stylesheet" href="{{ asset('/assets/frontuser/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/frontuser/css/owl.carousel.min.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/frontuser/css/owl.theme.default.min.css') }}" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
-    <link href="{{ asset('style.css')}}" rel="stylesheet">
+    <link href="{{ asset('style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <link href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css" rel="stylesheet">
 
-    <link href="{{ asset('/assets/frontuser/css/jquery.datetimepicker.min.css')}}" rel="stylesheet">
-    <link href="{{ asset('/assets/frontuser/css/jquery.datetimepicker.min.css')}}" rel="stylesheet" type="text/css" media="all">
+    <link href="{{ asset('/assets/frontuser/css/jquery.datetimepicker.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/assets/frontuser/css/jquery.datetimepicker.min.css') }}" rel="stylesheet" type="text/css"
+        media="all">
 
 
 </head>
 
 
 <body>
-<div class="container">
-<div class="row hdrrow ">
-    <div class="col-md-3 col-3 nav_logo">
-        <a class="navbar-brand" href="/">
-            <img src="{{ asset('/assets/frontuser/images/dora_cabs1.png')}}" alt="Dora cabs">
-        </a>
-    </div>
-    <div class="col-md-9 col-9 nav_bar">
-
-        <nav class="navbar navbar-expand-sm pb-lg-4 pb-0 pt-lg-4 pt-3 " id="top-menu">
-            <div class="container-fluid">
-
-                <button class="navbar-toggler" id="toggle_icon" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"><i class="fa fa-bars" aria-hidden="true"></i></span>
-  </button>
-                <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                    <ul class="navbar-nav">
-                    <li><a href="{{ route('home') }}" class="nav-link" >Home</a></li>
-                    <li><a href="{{ route('about') }}" class="nav-link" >About</a></li>
-                    <li><a href="{{ route('tariff') }}" class="nav-link" >Tariff</a></li>
-                    <li><a href="{{ route('contact') }}" class="nav-link" >Contact</a></li>
-                    </ul>
-                </div>
-                <a href="tel:+91 8015521177" class="btn" id="top_call"><i class="fa fa-phone" aria-hidden="true"></i>
-                +91 8015521177 </a>
+    <div class="container">
+        <div class="row hdrrow ">
+            <div class="col-md-3 col-3 nav_logo">
+                <a class="navbar-brand" href="/">
+                    <img src="{{ asset('/assets/frontuser/images/dora_cabs1.png') }}" alt="Dora cabs">
+                </a>
             </div>
+            <div class="col-md-9 col-9 nav_bar">
 
-        </nav>
+                <nav class="navbar navbar-expand-sm pb-lg-4 pb-0 pt-lg-4 pt-3 " id="top-menu">
+                    <div class="container-fluid">
+
+                        <button class="navbar-toggler" id="toggle_icon" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#collapsibleNavbar">
+                            <span class="navbar-toggler-icon"><i class="fa fa-bars" aria-hidden="true"></i></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                            <ul class="navbar-nav">
+                                <li><a href="{{ route('home') }}" class="nav-link">Home</a></li>
+                                <li><a href="{{ route('about') }}" class="nav-link">About</a></li>
+                                <li><a href="{{ route('tariff') }}" class="nav-link">Tariff</a></li>
+                                <li><a href="{{ route('contact') }}" class="nav-link">Contact</a></li>
+                            </ul>
+                        </div>
+                        <a href="tel:+91 8015521177" class="btn" id="top_call"><i class="fa fa-phone"
+                                aria-hidden="true"></i>
+                            +91 8015521177 </a>
+                    </div>
+
+                </nav>
+            </div>
+        </div>
     </div>
-</div>
-</div>
 
 
 </body>
+
 </html>
