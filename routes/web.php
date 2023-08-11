b13e90c661d76783158d33b7a06884aaa2b0d1d9
Spider9813 <johnwilliams.duskcoders@gmail.com>
2023-08-11 10:26:18 +0530

    admin

--- routes/web.php
+++ routes/web.php
@@ -61,11 +61,3 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\AdminHomeController::class, 'index'])->name('home');
 Route::get('dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
-
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
